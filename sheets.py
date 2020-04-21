0bc79898a9f84e0c6480ae54d9c8c5a1f2462bff
Nicolas VERGNAC <nvergnac@macbook-air-de-nicolas.home>
2020-04-21 17:32:25 +0200

    lead project 1.0

---
+++ sheets.py
@@ -0,0 +1,139 @@
+import gspread
+import argparse
+import sys
+import random
+from pprint import pprint
+from datetime import datetime, timedelta
+from operator import itemgetter
+import csv
+
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+from utils import utils_fct
+
+
+def init():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        "credentials.json", scope)
+    gspread_client = gspread.authorize(creds)
+    return gspread_client
+
+
+def get_client_data(gspread_client, client_name):
+    sheet = gspread_client.open("Base donnée client Leads").sheet1
+    client_database = sheet.get_all_records()
+    client_data = []
+    for client in client_database:
+        if client_name == client['customerName']:
+            print(client['customerName'])
+            client_data.append(client)
+    if len(client_data) == 1:
+        return client_data[0]
+    elif len(client_data) > 1:
+        print("Il y a {} sources de lead disponibles :\n".format(len(client_data)))
+        for i, customer in enumerate(client_data, 1):
+            print("{} : {}".format(i, customer['leadSource']))
+        leadsource_nb = int(
+            input("Entrez le numéro de la source que vous souhaitez utiliser:\n"))
+        try:
+            return client_data[leadsource_nb - 1]
+        except IndexError as error:
+            print("La valeur sélectionnée : {} semble incorrecte.\nErreur {}".format(
+                leadsource_nb, error))
+    return 0
+
+
+def write_lead_API(gspread_client, customer_data, lead_data):
+    print(customer_data)
+    lead_sheet = gspread_client.open('Leads distribués').sheet1
+    row_nb = utils_fct.get_insert_row_nb(lead_sheet)
+    for lead in lead_data:
+        print(lead)
+        utils_fct.insert_lead(lead_sheet, row_nb, lead, customer_data)
+        row_nb += 1
+        if row_nb > 4:
+            print(row_nb)
+            break
+        # exit(0)
+        #lead_sheet.update_cell(2, 1, lead_data['Date'])
+
+
+def write_lead_CSV(customer_data, lead_data, lead_nb, options):
+    csv_filename = 'lead-extract_' + \
+        customer_data['customerName'] + '_' + \
+        str(datetime.now().strftime("%d_%m_%Y_%H:%M:%S")) + '.csv'
+    with open(csv_filename, 'w') as f:
+        fieldnames = ['Date', '1) Isolation pour', '2) Quel(s) type(s) de surface à isoler ?',
+                      '3) Nom', '4) Prénom', '5) Code postal', '6) Numéro de téléphone', '7) Email', 'Sent']
+        thewriter = csv.DictWriter(f, fieldnames)
+        thewriter.writeheader()
+
+        assigned_lead = 0
+        lead_source = lead_data
+        if options.premium is True:
+            premium_lead_data = sorted(lead_data, key=itemgetter('Date'), reverse=True)
+            lead_source = premium_lead_data
+        elif options.rand is True:
+            random.shuffle(lead_data)
+            
+        for lead in lead_source:
+            lead['Sent'] = customer_data['customerName']
+            thewriter.writerow(lead)
+            assigned_lead += 1
+            if assigned_lead == lead_nb:
+                break
+
+
+def get_available_leads(gspread_client, customer_data, options):
+    valid_lead = []
+    available_leads = 0
+    print(customer_data['leadSource'])
+    sheet = gspread_client.open(customer_data['leadSource']).sheet1
+    ws = sheet.get_all_records()
+    postal_code_set = utils_fct.get_postal_code_set(customer_data)
+    for lead in ws:
+        if utils_fct.is_valid_lead(lead, customer_data, postal_code_set) == True:
+            valid_lead.append(lead)
+            available_leads += 1
+    print("Il y a {} leads disponibles, combien souhaitez-vous en extraire ?".format(available_leads))
+    lead_nb = int(
+        input("Entrez un nombre (max:{}): \n".format(available_leads)))
+    #write_lead_API(gspread_client, data, valid_lead)
+    write_lead_CSV(customer_data, valid_lead, lead_nb, options)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("client_name", type=str,
+                        help="Nom du client pour qui on veut extraire des leads.\n")
+    parser.add_argument(
+        "--premium", help="Selectionne les leads les plus récents.", action="store_true")
+    parser.add_argument(
+        "--rand", help="Selectionne les leads les plus anciens.", action="store_true")
+    return parser.parse_args()
+
+
+# def to_be_defined():
+ #   pprint(data[0]['leadsSource'])
+  #  sheet_mur_placo = client.open(data[0]['leadsSource']).sheet1
+   # data_mur_placo = sheet_mur_placo.get_all_records()
+    #numRow = sheet_mur_placo.row_count()
+
+    # print(len(data_mur_placo))
+
+
+if __name__ == "__main__":
+    options = get_args()
+    print(options.premium)
+    gspread_client = init()
+    customer_data = get_client_data(gspread_client, options.client_name)
+    if customer_data == 0:
+        sys.exit("Le client {} est introuvable dans la base de client.".format(
+            options.client_name))
+    print(customer_data)
+    get_available_leads(gspread_client, customer_data, options)
+
+    pass
---
+++ utils/utils_fct.py
@@ -0,0 +1,76 @@
+from datetime import datetime, timedelta
+import gspread
+
+
+def get_datetime(date):
+    try:
+        datetime_object = datetime.strptime(date, '%m/%d/%y %H:%M')
+    except ValueError as ve:
+        #print('ValueError Raised:', ve)
+        datetime_object = datetime.strptime(date, '%m/%d/%y %I:%M%p')
+        # print(datetime_object)
+    return datetime_object
+
+
+def is_valid_lead(lead_row, customer_data, postal_code_set):
+    datetime_date = get_datetime(lead_row['Date'])
+    if datetime_date > (datetime.now() - timedelta(days=3)) and lead_row['Sent'] == '':
+        if postal_code_set != 0:
+            postal_code = get_postal_code_set(lead_row['5) Code postal'])
+            if postal_code in postal_code_set:
+                return True
+            else:
+                return False
+        return True
+    return False
+
+
+def get_postal_code(postal_code):
+    if postal_code.isnumeric() is True:
+        if len(postal_code) == 4:
+            return int(postal_code[0:1])
+        elif len(postal_code) == 5:
+            return int(postal_code[0:2])
+    return 0
+
+
+def get_postal_code_set(customer_data):
+    if customer_data['region'] != '':
+        if type(customer_data['region']) == int:
+            postal_code_set = set([customer_data['region']])
+        else:
+            postal_code_list = customer_data['region'].split(',')
+            postal_code_list = [int(i) for i in postal_code_list]
+            postal_code_set = set(postal_code_list)
+        return postal_code_set
+    else:
+        return 0
+
+
+def insert_lead(sheet, row, lead, customer_data):
+    sheet.update_cell(row, 1, lead['Date'])
+    sheet.update_cell(row, 2, lead['1) Isolation pour'])
+    sheet.update_cell(row, 3, lead['2) Quel(s) type(s) de surface à isoler ?'])
+    sheet.update_cell(row, 4, lead['3) Nom'])
+    sheet.update_cell(row, 5, lead['4) Prénom'])
+    sheet.update_cell(row, 6, lead['5) Code postal'])
+    sheet.update_cell(row, 7, lead['6) Numéro de téléphone'])
+    sheet.update_cell(row, 8, lead['7) Email'])
+    sheet.update_cell(row, 9, customer_data['customerName'])
+
+
+def get_insert_row_nb(sheet):
+    lead_ws = sheet.get_all_records()
+    row_nb = 2
+    for row_nb, row_value in enumerate(lead_ws, 0):
+        if row_value['Date'] is not None:
+            print("Valeur : {} + ' a la ligne : {}".format(row_value, row_nb))
+            insert_row += 1
+    return row_nb
+
+
+if __name__ == "__main__":
+    code = get_postal_code('88360')
+    print(code)
+    print(type(code))
+    pass
